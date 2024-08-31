ec00925a6426b0e85bdb9583da673cfebdd7f10f
LmOpe <lawalmuhammedope@gmail.com>
2024-08-31 06:36:02 +0100

    Create user model for authentication

--- ecoride/settings.py
+++ ecoride/settings.py
@@ -10,10 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
-#import os
-
+# Load environment variables from .env file
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-uz(lpwaad74e534g2n!zz3w0a-084^)@(hbu0lm+hj4jjqwjrd'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG")
 
 ALLOWED_HOSTS = []
 
@@ -80,11 +82,11 @@ WSGI_APPLICATION = 'ecoride.wsgi.application'
 DATABASES = {
      "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": 'ecoride',
-        "USER": 'muhammed',
-        "PASSWORD": '@Muhammed35',
-        "HOST": 'localhost',
-        "PORT": "5432",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
     }
 }
 
--- users/models.py
+++ users/models.py
@@ -1,3 +1,85 @@
-from django.db import models
+"""
+The Module for user authentication related models
+"""
 
-# Create your models here.
+import uuid
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+
+# Role choices
+ROLE_CHOICES = (
+    ('Admin', 'Admin'),
+    ('Rider', 'Rider'),
+    ('User', 'User'),
+)
+
+class UserManager(BaseUserManager):
+    """Model manager for the User model"""
+
+    def create_user(self, email=None, phone=None, password=None, **kwargs):
+        """Function for creating user instance"""
+        if email is None:
+            raise TypeError('User must have an email address')
+        if phone is None:
+            raise TypeError('User must have a phone number')
+        if password is None:
+            raise TypeError('User must have a password')
+
+        user = self.model(email=self.normalize_email(email),
+                          phone=phone,
+                          **kwargs,
+                          )
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, phone, password, **kwargs):
+        """Super user function"""
+
+        user = self.create_user(email, phone, password, **kwargs)
+        user.is_superuser = True
+        user.is_staff = True
+        user.is_active = True
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """The model for storing auth user details"""
+
+    id = models.UUIDField(primary_key=True, 
+                          default=uuid.uuid4, 
+                          editable=False)
+    email = models.EmailField(db_index=True, unique=True)
+    phone = models.CharField(max_length=15, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
+    is_superuser = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+
+    # Adding related_name to avoid reverse accessor clashes
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='custom_user_set', 
+        blank=True,
+        help_text='The groups this user belongs to.'
+    )
+
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='custom_user_permissions_set',
+        blank=True,
+        help_text='Specific permissions for this user.'
+    )
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['phone']
+
+    def __str__(self):
+        """Function to provide human-readable string for the object"""
+        return f'{self.id} {self.email}'
