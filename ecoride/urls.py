c5f5a3242084b212010db3fb17798e1a91dbf1ca
LmOpe <lawalmuhammedope@gmail.com>
2024-09-02 17:52:29 +0100

    Add documentation logic using swagger

--- ecoride/urls.py
+++ ecoride/urls.py
@@ -17,7 +17,28 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from rest_framework import permissions
+
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Ecoride API",
+      default_version='v1',
+      description="API documentation for Ecoride",
+      terms_of_service="https://www.ecoride.com/terms/",
+      contact=openapi.Contact(email="contact@ecoride.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/auth/', include("users.urls")),
+    path('docs/swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
