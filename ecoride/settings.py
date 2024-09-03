e7e3effa09a1dafaae29b3c0c86602cde628eb4c
LmOpe <lawalmuhammedope@gmail.com>
2024-09-03 14:18:10 +0100

    Add static settings

--- ecoride/settings.py
+++ ecoride/settings.py
@@ -128,6 +128,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'ecoride', 'static'),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
